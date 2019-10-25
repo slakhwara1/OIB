bbbfc3c5582dde7d8bfe50f4549634b93ab8acf8
Shivam Lakhwara <shivam.lakhwara@mavenwave.com>
2019-10-25 16:20:53 +0530

    Minor formatting

--- PRACTICE/src/PRAC/Solution.java
+++ PRACTICE/src/PRAC/Solution.java
@@ -9,17 +9,23 @@ public class Solution {
 		for (int[] l : memo) {
 			Arrays.fill(l, -1);
 		}
+		System.out.println("FIRST CALL : func( " + printArray(nums) + " , " + "-1" + " , " + 0 + " , " + " memo )");
 		return lengthofLIS(nums, -1, 0, memo);
 	}
 
 	public int lengthofLIS(int[] nums, int previndex, int curpos, int[][] memo) {
 		print2dArray(memo);
 		if (curpos == nums.length) {
+			
+			System.out.println("------------------------------------------------------------------");
+			System.out.println("POPPED : func( " + printArray(nums) + " , " + previndex + " , " + curpos + " , " + " memo )" + "returned = "+0);
+			System.out.println("------------------------------------------------------------------");
+			
 			return 0;
 		}
 		if (memo[previndex + 1][curpos] >= 0) {
 			System.out.println("------------------------------------------------------------------");
-			System.out.println("POPPED : func( " + printArray(nums) + " , " + previndex + " , " + curpos + " , " + " memo )");
+			System.out.println("POPPED : func( " + printArray(nums) + " , " + previndex + " , " + curpos + " , " + " memo )"+ "returned = "+memo[previndex + 1][curpos]);
 			System.out.println("------------------------------------------------------------------");
 			return memo[previndex + 1][curpos];
 		}
@@ -33,7 +39,15 @@ public class Solution {
 		System.out.println("BEFORE NOT TAKEN : func( " + printArray(nums) + " , " + previndex + " , " + curpos + " , " + " memo )");
 		int nottaken = lengthofLIS(nums, previndex, curpos + 1, memo);
 		System.out.println("AFTER NOT TAKEN : func( " + printArray(nums) + " , " + previndex + " , " + curpos + " , " + " memo )");
+
+		System.out.println("TAKEN = "+taken+" NOT_TAKEN = "+nottaken);
 		memo[previndex + 1][curpos] = Math.max(taken, nottaken);
+		
+
+		System.out.println("------------------------------------------------------------------");
+		System.out.println("-->POPPED : func( " + printArray(nums) + " , " + previndex + " , " + curpos + " , " + " memo )"+ "returned = "+memo[previndex + 1][curpos]);
+		System.out.println("------------------------------------------------------------------");
+		
 		return memo[previndex + 1][curpos];
 	}
 	
@@ -46,8 +60,12 @@ public class Solution {
 	
 	public void print2dArray(int [][] A) {
 		for(int[] array : A) {
-			for(int item : array) 
-				System.out.print(item + " ");
+			for(int item : array) {
+				if (item < 0 || item > 9)
+					System.out.print(item+" ");
+				else
+					System.out.print(item + "  ");
+			}
 			System.out.println();
 		}
 	}
