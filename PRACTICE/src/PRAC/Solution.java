8a28a47583598259b5fadc9b9091662babaf1f35
Shivam Lakhwara <shivam.lakhwara@mavenwave.com>
2019-11-15 18:14:10 +0530

    Min_jumps_array_memoization

--- PRACTICE/src/PRAC/Solution.java
+++ PRACTICE/src/PRAC/Solution.java
@@ -1,77 +1,40 @@
 package PRAC;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Solution {
-	public int lengthOfLIS(int[] nums) {
-		int memo[][] = new int[nums.length + 1][nums.length];
-		for (int[] l : memo) {
-			Arrays.fill(l, -1);
-		}
-		System.out.println("FIRST CALL : func( " + printArray(nums) + " , " + "-1" + " , " + 0 + " , " + " memo )");
-		return lengthofLIS(nums, -1, 0, memo);
+	public int jump(int[] A) {
+		int[] memo = new int[A.length];
+		Arrays.fill(memo, -1);
+		int ans = jumpHelper(memo, A, 0);
+		return ans == Integer.MAX_VALUE ? -1 : ans;
 	}
 
-	public int lengthofLIS(int[] nums, int previndex, int curpos, int[][] memo) {
-		print2dArray(memo);
-		if (curpos == nums.length) {
-			
-			System.out.println("------------------------------------------------------------------");
-			System.out.println("POPPED : func( " + printArray(nums) + " , " + previndex + " , " + curpos + " , " + " memo )" + "returned = "+0);
-			System.out.println("------------------------------------------------------------------");
-			
+	public int jumpHelper(int[] memo, int[] A, int index) {
+
+		if (index >= A.length - 1)
 			return 0;
-		}
-		if (memo[previndex + 1][curpos] >= 0) {
-			System.out.println("------------------------------------------------------------------");
-			System.out.println("POPPED : func( " + printArray(nums) + " , " + previndex + " , " + curpos + " , " + " memo )"+ "returned = "+memo[previndex + 1][curpos]);
-			System.out.println("------------------------------------------------------------------");
-			return memo[previndex + 1][curpos];
-		}
-		int taken = 0;
-		if (previndex < 0 || nums[curpos] > nums[previndex]) {
-			System.out.println("BEFORE TAKEN : func( " + printArray(nums) + " , " + previndex + " , " + curpos + " , " + " memo )");
-			taken = 1 + lengthofLIS(nums, curpos, curpos + 1, memo);
-			System.out.println("AFTER TAKEN : func( " + printArray(nums) + " , " + previndex + " , " + curpos + " , " + " memo )");
-		}
 
-		System.out.println("BEFORE NOT TAKEN : func( " + printArray(nums) + " , " + previndex + " , " + curpos + " , " + " memo )");
-		int nottaken = lengthofLIS(nums, previndex, curpos + 1, memo);
-		System.out.println("AFTER NOT TAKEN : func( " + printArray(nums) + " , " + previndex + " , " + curpos + " , " + " memo )");
+		if (memo[index] != -1)
+			return memo[index];
 
-		System.out.println("TAKEN = "+taken+" NOT_TAKEN = "+nottaken);
-		memo[previndex + 1][curpos] = Math.max(taken, nottaken);
-		
+		int minimumJumps = Integer.MAX_VALUE;
 
-		System.out.println("------------------------------------------------------------------");
-		System.out.println("-->POPPED : func( " + printArray(nums) + " , " + previndex + " , " + curpos + " , " + " memo )"+ "returned = "+memo[previndex + 1][curpos]);
-		System.out.println("------------------------------------------------------------------");
-		
-		return memo[previndex + 1][curpos];
-	}
-	
-	public String printArray(int[] A) {
-		String s = "[ ";
-		for(int item : A)
-			s+=""+item+" ";
-		return s+" ]";
-	}
-	
-	public void print2dArray(int [][] A) {
-		for(int[] array : A) {
-			for(int item : array) {
-				if (item < 0 || item > 9)
-					System.out.print(item+" ");
-				else
-					System.out.print(item + "  ");
-			}
-			System.out.println();
+		for (int i = 1; i <= A[index]; i++) {
+			minimumJumps = Integer.min(minimumJumps, 1 + jumpHelper(memo, A, index + i));
 		}
+		memo[index] = minimumJumps;
+		return minimumJumps;
 	}
-	
-	
+
 	public static void main(String[] args) {
-		System.out.println(new Solution().lengthOfLIS(new int[] { 10, 22, 9, 33, 21, 50, 41, 60, 80 }));
+//		int[] A = new int[] { 2, 3, 1, 1, 4 };
+		int[] A = new int[] { 33, 21, 50, 0, 0, 46, 34, 3, 0, 12, 33, 0, 31, 37, 15, 17, 34, 18, 0, 4, 12, 41, 18, 35,
+				37, 34, 0, 47, 0, 39, 32, 49, 5, 41, 46, 26, 0, 2, 49, 35, 4, 19, 2, 27, 23, 49, 19, 38, 0, 33, 47, 1,
+				21, 36, 18, 33, 0, 1, 0, 39, 0, 22, 0, 9, 36, 45, 31, 4, 14, 48, 2, 33, 0, 39, 0, 37, 48, 44, 0, 11, 24,
+				16, 10, 23, 22, 41, 32, 14, 22, 16, 23, 38, 42, 16, 15, 0, 39, 23, 0, 42, 15, 25, 0, 41, 2, 48, 28 };
+
+		System.out.println(new Solution().jump(A));
 	}
+
 }
